b504eb63747b15ca6bcd80da1edb1e8ea04e0ddd
Sho Iwasaki <m.sho.a.engineer@gmail.com>
2023-02-25 09:56:36 +0900

    最初に開かれるページをappに変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:user_manage_qrcode/Page/SignIn/SignIn_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:user_manage_qrcode/app.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SignIn(),
+      home: App(),
     );
   }
 }

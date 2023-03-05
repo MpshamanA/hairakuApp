e377cd6c8e5700baabe49cf647445b9b92c99190
Sho Iwasaki <m.sho.a.engineer@gmail.com>
2023-03-05 20:03:12 +0900

    会議情報UIをfirestoreに接続して作成するように修正

--- lib/Page/Home/Home_page.dart
+++ lib/Page/Home/Home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:user_manage_qrcode/Page/AccountSetting/AccountSetting_page.dart';
@@ -5,61 +8,14 @@ import 'package:user_manage_qrcode/Page/AccountSetting/AccountSetting_page.dart'
 // import '../../Component/AlertBuilderForCupertino.dart';
 import '../../Const//ConstColors.dart';
 import '../../Custom/CustomMaterialPageRoute.dart';
-import '../../Model/Customer.dart';
+import '../../Component/UserCard.dart';
+import '../../Component/MeetingCard.dart';
 import '../../Providers/userInfo_provider.dart';
 // import '../../Providers/userManage_provider.dart';
 
-class _UserCard extends StatelessWidget {
-  _UserCard(
-      {Key? key,
-      required this.mainColor,
-      required this.customer,
-      required this.deviceSize})
-      : super(key: key);
+import 'package:intl/intl.dart';
 
-  final Customer customer;
-  final Color mainColor;
-  final Size deviceSize;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        height: deviceSize.height * 0.28,
-        width: deviceSize.width * 0.9,
-        child: Card(
-          color: mainColor, // Card自体の色
-          elevation: 8, // 影の離れ具合
-          shadowColor: Colors.black, // 影の色
-          shape: RoundedRectangleBorder(
-            // 枠線を変更できる
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Center(
-            child: SizedBox(
-              child: ListTile(
-                title: Text(
-                  customer.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                // subtitle: Text(
-                //   customer.companyName,
-                //   textAlign: TextAlign.center,
-                //   style: const TextStyle(color: Colors.white70),
-                // ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+import 'dart:convert';
 
 class Home extends ConsumerStatefulWidget {
   Home({Key? key}) : super(key: key);
@@ -68,15 +24,8 @@ class Home extends ConsumerStatefulWidget {
 }
 
 class _HomeState extends ConsumerState<Home> {
-  //FireBaseからmeetings情報をとってくる
-  List<Map<String, dynamic>> meetings = [
-    {'id': 1, '会議名': '会議１', '開催者': '開催者１', '開催日': '23-5-24'},
-    {'id': 2, '会議名': '会議２', '開催者': '開催者２', '開催日': '23-5-24'},
-    {'id': 3, '会議名': '会議３', '開催者': '開催者３', '開催日': '23-5-24'},
-    {'id': 4, '会議名': '会議４', '開催者': '開催者４', '開催日': '23-5-24'},
-    {'id': 5, '会議名': '会議５', '開催者': '開催者５', '開催日': '23-5-24'},
-    {'id': 6, '会議名': '会議６', '開催者': '開催者６', '開催日': '23-5-24'},
-  ];
+  List<Map<String, dynamic>> meetingsd = [];
+  late String uid = ref.watch(userProvider).uid;
 
   @override
   Widget build(BuildContext context) {
@@ -110,32 +59,34 @@ class _HomeState extends ConsumerState<Home> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          _UserCard(
+          UserCard(
             mainColor: ConstColors.mainColor,
             customer: ref.watch(userProvider),
             deviceSize: deviceSize,
           ),
-          Text(
-            '参加した最新の集まり',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          //参加してる
-          ref.watch(userProvider).isAdmission
-              ?
-              //データをとる時に最初の会議は削除する
-              MeetingCardWidget(
-                  deviceWidth: deviceWidth,
-                  meetingName: meetings[0]['会議名'],
-                  startDate: meetings[0]['開催日'],
-                  organizer: meetings[0]['開催者'])
+          // Text(
+          //   '参加した最新の集まり',
+          //   style: TextStyle(fontWeight: FontWeight.bold),
+          // ),
+          // //参加してる
+          // ref.watch(userProvider).isAdmission
+          //     ?
+          //     //データをとる時に最初の会議は削除する
+          //     MeetingCard(
+          //         deviceWidth: deviceWidth,
+          //         meetingName: meetings[0]['会議名'],
+          //         startDate: meetings[0]['開催日'],
+          //         organizer: meetings[0]['開催者'],
+          //       )
 
-              // : Text('入場ミーティングは無し')
-              // : Text('現在参加していません'),
-              : MeetingCardWidget(
-                  deviceWidth: deviceWidth,
-                  meetingName: meetings[0]['会議名'],
-                  startDate: meetings[0]['開催日'],
-                  organizer: meetings[0]['開催者']),
+          //     // : Text('入場ミーティングは無し')
+          //     // : Text('現在参加していません'),
+          //     : MeetingCard(
+          //         deviceWidth: deviceWidth,
+          //         meetingName: meetings[0]['会議名'],
+          //         startDate: meetings[0]['開催日'],
+          //         organizer: meetings[0]['開催者'],
+          //       ),
           //入場中の会議
           Text(
             "参加した過去の集まり",
@@ -146,105 +97,68 @@ class _HomeState extends ConsumerState<Home> {
             height: deviceHeight * 0.15,
             width: deviceWidth * 0.7,
             child: Scrollbar(
-              child: ListView.builder(
-                  // shrinkWrap: true,
-                  // itemCount: meetings.length,
-                  itemCount: 5,
-                  itemBuilder: (c, i) {
-                    //データをとる時に最初の会議は削除する
-                    return SizedBox(
-                      height: deviceHeight * 0.09,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          MeetingCardWidget(
-                              deviceWidth: deviceWidth,
-                              meetingName: meetings[i]['会議名'],
-                              startDate: meetings[i]['開催日'],
-                              organizer: meetings[i]['開催者']),
-                        ],
-                      ),
-                    );
+              //firestoreからデータを取得して表示する
+              child: StreamBuilder<DocumentSnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('users')
+                      .doc(uid)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    } else if (snapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return Text('Loading...');
+                    }
+                    final data = snapshot.data!.data() as Map<String, dynamic>;
+                    final meetings = data['meetingsObj'] as List<dynamic>;
+
+                    return ListView.builder(
+                        //firestoreから取得したデータは最大５件まで表示する
+                        itemCount: meetings.length >= 5 ? 5 : meetings.length,
+                        itemBuilder: (_, i) {
+                          //データをとる時に最初の会議は削除する
+                          return SizedBox(
+                            height: deviceHeight * 0.09,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                MeetingCard(
+                                    deviceWidth: deviceWidth,
+                                    meetingName: meetings[i]['meetingName'],
+                                    startDate: meetings[i]['startDate'],
+                                    organizer: meetings[i]['organizer']),
+                              ],
+                            ),
+                          );
+                        });
                   }),
             ),
           ),
           TextButton(
-              onPressed: () {
+              onPressed: () async {
+                //テストで会議を追加してるがこれはQRコードを読み込んだときに実装する
+                await FirebaseFirestore.instance
+                    .collection('users')
+                    .doc(uid)
+                    .update({
+                  'meetingsObj': FieldValue.arrayUnion([
+                    {
+                      'id': "iodhjfposdjfpds",
+                      'meetingName': 'アロハ会議',
+                      'organizer': 'アロハ会社',
+                      'startDate': DateFormat('yy-MM-dd').format(DateTime.now())
+                    }
+                  ])
+                });
+
+                // await getMeetingsObj();
                 // Navigator.push(
                 //     context,
                 //     CustomMaterialPageRoute(
                 //         builder: (context) => const SignUp()));
               },
               child: const Text('もっと見る'))
-        ],
-      ),
-    );
-  }
-}
-
-class MeetingCardWidget extends StatelessWidget {
-  const MeetingCardWidget(
-      {Key? key,
-      required this.deviceWidth,
-      required this.meetingName,
-      required this.startDate,
-      required this.organizer})
-      : super(key: key);
-
-  final double deviceWidth;
-  final String meetingName;
-  final String startDate;
-  final String organizer;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      width: deviceWidth * 0.65,
-      // color: Colors.blueAccent,
-      decoration: BoxDecoration(
-          // 枠線
-          border:
-              Border.all(color: Color.fromARGB(130, 116, 192, 255), width: 2),
-          // 角丸
-          borderRadius: BorderRadius.circular(8),
-          color: Color.fromARGB(52, 116, 192, 255)),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-
-        // crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SizedBox(
-                    width: deviceWidth * 0.4,
-                    child: Text(
-                      meetingName,
-                      // style: TextStyle(fontWeight: FontWeight.bold),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 5),
-                    child: Text(
-                      startDate,
-                      style: TextStyle(fontSize: 12, color: Colors.blueGrey),
-                    ),
-                  ),
-                ],
-              ),
-              Text(
-                organizer,
-                style: TextStyle(fontSize: 12),
-              ),
-              // Text('23-5-24'),
-            ],
-          ),
         ],
       ),
     );
